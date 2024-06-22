83516bb1785b307ad61df434f1b74b52fa1a9802
Wix <shendrickwilliams@gmail.com>
2024-06-22 10:57:17 +0200

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+import java.util.ArrayList;
+
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        new Start().start();
+    }
+}
